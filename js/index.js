6361fb16110a8414c487fb1faefbbcb4dfd8bd1d
hoangquoclk <hoangquoclk003@gmail.com>
2020-09-06 12:59:41 +0700

    upload ngay 12h 6/9

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 	$('.fa-search').click(function(){
 		$('.navbar').css('display','none');
+		$('#search-hide').css('display','block');
+		$('#search-text').val('');
+	});
+
+	$('#close').click(function(){
+		$('#search-hide').css('display','none');
+		$('.navbar').css('display','block');
+	});
+
+	// thong tin quan coffee
+	$('.fa-list').click(function(){
+		$('#information').css('display','block');
+		// $('#information').css('visibility','visible');
+	});
+
+	$('#close-info').click(function(){
+    	// $("#information").animate({right: '-400px'}, "slow");
+    	// $('#information').css('visibility','hidden');
+    	// HideTheElementAfterAnimation();
+    	$('#information').css('display','none');
 	});
 });
